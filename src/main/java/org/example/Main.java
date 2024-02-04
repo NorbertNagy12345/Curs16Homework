c18f5f7465cb81fc5533f88bcb03f0174eb6299b
NorbertNagy12345 <norbert.nagy.comp@gmail.com>
2024-02-04 12:19:21 +0200

    New Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        CountryReader countryReader = new CountryReader(src/main/resources/countries.txt);
+        CountryReader countryReader = new CountryReader("src/main/resources/countries.txt");
 
 
         CountryService countryService = countryReader.getCountryService();
